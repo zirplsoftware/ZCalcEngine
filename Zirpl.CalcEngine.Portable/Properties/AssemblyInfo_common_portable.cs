d45df5f694a2f8534a8da44d35d8631f88ff027d
nlafratta <nlafratta@zirpl.com>
2014-06-27 20:19:33 -0400

    fixing assembly info

--- Zirpl.CalcEngine.Portable/Properties/AssemblyInfo_common_portable.cs
+++ Zirpl.CalcEngine.Portable/Properties/AssemblyInfo_common_portable.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zirpl Software")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0 ")]
+[assembly: AssemblyInformationalVersion("1.0")]
